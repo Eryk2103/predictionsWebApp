8d8423f105f5c0ef81d12d74fe88bcbda246a0cd
Eryk Ciesielski <eryk@9903@gmail.com>
2024-10-17 14:10:54 +0200

    add last week predictions section

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -9,22 +9,36 @@
         <div>12.03</div>
         <ul class="games">
             <li class="games__item">
-                13:00 Manchester United - Liverpool 
+                <div class="info">13:00 Manchester United - Liverpool</div> 
             </li>
             <li class="games__item">
-                16:00 Aston Villa - Brentford (1:0)
+                <div class="info">16:00 Aston Villa - Brentford</div> <div class="prediction">1:0</div>
             </li>
             <li class="games__item">
-                16:00 Newcastle United - Arsenal 
+                <div class="info">16:00 Newcastle United - Arsenal </div>
             </li>
         </ul>
         <div>13.03</div>
         <ul>
             <li class="games__item">
-                14.30 Everton - Manchester City
+                <div class="info">14.30 Everton - Manchester City</div>
             </li>
             <li class="games__item">
-                14:30 Brighton - Chelsea
+                <div class="info">14:30 Brighton - Chelsea</div>
+            </li>
+        </ul>
+    </section>
+    <section>
+        <h2>Last week predictions</h2>
+        <ul>
+            <li class="games__item">
+                <div class="info">Manchester City - Everton 3-0 </div><div class="prediction">5-1</div>
+            </li>
+            <li class="games__item">
+                <div class="info">Manchester United - Chelsea 6-5</div> 
+            </li>
+            <li class="games__item">
+                 <div class="info">Brighton - Fullham 3-1</div> <div class="prediction">3-1</div>
             </li>
         </ul>
     </section>
@@ -33,9 +47,10 @@
 <style scoped>
     .games__item{
         border: solid 1px black;
-        padding: 1em;
         border-radius: .3rem;
         margin: .5rem 0;
+        display: flex;
+        justify-content: space-between;
     }
 
     .btn{
@@ -45,5 +60,14 @@
         font-size: 1rem;
         border-radius: .3rem;
         border: solid 1px black;
+    }
+
+    .prediction{
+        border-left: solid 1px gray;
+        padding: 1em;
+    }
+
+    .info{
+        padding: 1em;
     }
 </style>
