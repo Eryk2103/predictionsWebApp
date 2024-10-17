bb509ed72395ea7cd9f3c5a1da70537afd8f0784
Eryk Ciesielski <eryk@9903@gmail.com>
2024-10-17 13:23:31 +0200

    create markup for upcoming games section

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+    <section>
+        <h2>Upcoming games</h2>
+        <div>12.03</div>
+        <ul>
+            <li>
+                <div>13:00 Manchester United - Liverpool</div> <button>predict</button>
+            </li>
+            <li>
+                <div>16:00 Aston Villa - Brentford</div> <button>predict</button>
+            </li>
+            <li>
+                <div>16:00 Newcastle United - Arsenal</div> <button>predict</button>
+            </li>
+        </ul>
+        <div>13.03</div>
+        <ul>
+            <li>
+                <div>14.30 Everton - Manchester City</div> <button>predict</button>
+            </li>
+            <li>
+                <div>14:30 Brighton - Chelsea</div> <button>predict</button>
+            </li>
+        </ul>
+    </section>
+</template>
