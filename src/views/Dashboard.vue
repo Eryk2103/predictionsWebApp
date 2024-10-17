fbe6c1b54b83ec2c99f175c78b6c6c85f83f1397
Eryk Ciesielski <eryk@9903@gmail.com>
2024-10-17 13:41:12 +0200

    add basic styles to upcoming games section

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,26 +5,45 @@
 <template>
     <section>
         <h2>Upcoming games</h2>
+        <button class="btn">predict all</button>
         <div>12.03</div>
-        <ul>
-            <li>
-                <div>13:00 Manchester United - Liverpool</div> <button>predict</button>
+        <ul class="games">
+            <li class="games__item">
+                13:00 Manchester United - Liverpool 
             </li>
-            <li>
-                <div>16:00 Aston Villa - Brentford</div> <button>predict</button>
+            <li class="games__item">
+                16:00 Aston Villa - Brentford (1:0)
             </li>
-            <li>
-                <div>16:00 Newcastle United - Arsenal</div> <button>predict</button>
+            <li class="games__item">
+                16:00 Newcastle United - Arsenal 
             </li>
         </ul>
         <div>13.03</div>
         <ul>
-            <li>
-                <div>14.30 Everton - Manchester City</div> <button>predict</button>
+            <li class="games__item">
+                14.30 Everton - Manchester City
             </li>
-            <li>
-                <div>14:30 Brighton - Chelsea</div> <button>predict</button>
+            <li class="games__item">
+                14:30 Brighton - Chelsea
             </li>
         </ul>
     </section>
 </template>
+
+<style scoped>
+    .games__item{
+        border: solid 1px black;
+        padding: 1em;
+        border-radius: .3rem;
+        margin: .5rem 0;
+    }
+
+    .btn{
+        width: 100%;
+        padding: .8em 1em; 
+        margin: 1rem 0;
+        font-size: 1rem;
+        border-radius: .3rem;
+        border: solid 1px black;
+    }
+</style>
